6f1f3823f6690b5edc07c670809b94570a65a331
dgoldsb <dgoldsb@live.nl>
2022-12-01 09:54:55 +0100

    Day 1 in Python oneliners

--- pyo3/aoc/2022/day_1.py
+++ pyo3/aoc/2022/day_1.py
@@ -5,10 +5,12 @@ from aoc.helpers import time_it
 @time_it
 def part_a(input_: str):
     """Part a?"""
-    return ""
+    return max(sum(int(y) for y in x.split("\n")) for x in input_.split("\n\n"))
 
 
 @time_it
 def part_b(input_: str):
     """Part b."""
-    return ""
+    return sum(
+        sorted(sum(int(y) for y in x.split("\n")) for x in input_.split("\n\n"))[-3:]
+    )
