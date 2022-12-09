c95d322bedc7a6251e1c43d174d260ab297c807d
dgoldsb <dgoldsb@live.nl>
2022-12-09 12:39:25 +0100

    Day 9

---
+++ pyo3/aoc/2022/day_9.py
@@ -0,0 +1,80 @@
+"""Solve day 9."""
+from aoc.helpers import time_it
+
+
+def _get_mod(diff: int, contract: bool):
+    if contract:
+        if abs(diff) // 2 == 0:
+            return 0
+        _mod = abs(diff) // 2
+    else:
+        if diff == 0:
+            return 0
+        _mod = abs(diff)
+    return int(_mod / abs(_mod)) * int(diff / abs(diff))
+
+
+class Rope:
+    def __init__(self, length=2):
+        self.__segment_locations = [(0, 0)] * length
+        self.tail_locations = set()
+
+    def _follow(self, index: int):
+        head = self.__segment_locations[index - 1]
+        tail = self.__segment_locations[index]
+        x_diff = head[0] - tail[0]
+        y_diff = head[1] - tail[1]
+
+        if abs(x_diff) > 1 or abs(y_diff) > 1:
+            if x_diff == 0 or y_diff == 0:
+                # With contraction.
+                contraction = True
+            else:
+                # Without contraction.
+                contraction = False
+            x_mod = _get_mod(x_diff, contraction)
+            y_mod = _get_mod(y_diff, contraction)
+
+            self.__segment_locations[index] = (tail[0] + x_mod, tail[1] + y_mod)
+
+    def _apply(self, instruction: chr):
+        match instruction:
+            case "U":
+                mod = (0, 1)
+            case "D":
+                mod = (0, -1)
+            case "L":
+                mod = (-1, 0)
+            case "R":
+                mod = (1, 0)
+            case _:
+                raise RuntimeError("Unknown operation!")
+        self.__segment_locations[0] = (
+            self.__segment_locations[0][0] + mod[0],
+            self.__segment_locations[0][1] + mod[1],
+        )
+        for i in range(1, len(self.__segment_locations)):
+            self._follow(i)
+        self.tail_locations.add(self.__segment_locations[-1])
+
+    def apply(self, instruction: chr, times: int):
+        for _ in range(times):
+            self._apply(instruction)
+
+
+@time_it
+def part_a(input_: str):
+    rope = Rope()
+    for line in input_.split("\n"):
+        op, num = line.split(" ")
+        rope.apply(op, int(num))
+    return len(rope.tail_locations)
+
+
+@time_it
+def part_b(input_: str):
+    rope = Rope(10)
+    for line in input_.split("\n"):
+        op, num = line.split(" ")
+        rope.apply(op, int(num))
+    return len(rope.tail_locations)
