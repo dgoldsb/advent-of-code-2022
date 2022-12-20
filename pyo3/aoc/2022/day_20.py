35beeb08da037c173618df34b86c3abc2c19b3e6
dgoldsb <dgoldsb@live.nl>
2022-12-20 14:14:54 +0100

    Finished day

--- pyo3/aoc/2022/day_20.py
+++ pyo3/aoc/2022/day_20.py
@@ -5,8 +5,8 @@ from copy import copy
 from aoc.helpers import time_it
 
 
-def _parse(input_: str) -> deque[tuple[int, int]]:
-    return deque((i, int(v)) for i, v in enumerate(input_.split("\n")))
+def _parse(input_: str, multiplier: int) -> deque[tuple[int, int]]:
+    return deque((i, int(v) * multiplier) for i, v in enumerate(input_.split("\n")))
 
 
 def _mix(q: deque, r: int):
@@ -20,19 +20,20 @@ def _put_first(q: deque, v):
     q.rotate(-1 * (rota - 1))
 
 
-@time_it
-def part_a(input_: str):
-    all_items = _parse(input_)
+def _solve(input_: str, multiplier: int = 1, mix_count: int = 1):
+    all_items = _parse(input_, multiplier)
 
     all_items_copy = copy(all_items)
 
+    # Mix.
     zero_item = None
-    for t in all_items_copy:
-        _put_first(all_items, t)
-        if (rotation := all_items[0][1]) != 0:
-            _mix(all_items, rotation)
-        else:
-            zero_item = t
+    for _ in range(mix_count):
+        for t in all_items_copy:
+            _put_first(all_items, t)
+            if (rotation := all_items[0][1]) != 0:
+                _mix(all_items, rotation)
+            else:
+                zero_item = t
 
     # Rotate to a zero.
     _put_first(all_items, zero_item)
@@ -40,11 +41,15 @@ def part_a(input_: str):
     result = 0
     for _ in range(3):
         all_items.rotate(-1000)
-        print(all_items[0][1])
         result += all_items[0][1]
     return result
 
 
 @time_it
+def part_a(input_: str):
+    return _solve(input_, 1, 1)
+
+
+@time_it
 def part_b(input_: str):
-    return None
+    return _solve(input_, 811589153, 10)
