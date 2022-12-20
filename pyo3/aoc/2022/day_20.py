19c4d3bd8e5a53654ab5812df2f7824b2819f59f
dgoldsb <dgoldsb@live.nl>
2022-12-20 14:08:50 +0100

    Complete rewrite to deque approach

--- pyo3/aoc/2022/day_20.py
+++ pyo3/aoc/2022/day_20.py
@@ -1,105 +1,48 @@
 """Solve day 20."""
-from dataclasses import dataclass
+from collections import deque
+from copy import copy
 
 from aoc.helpers import time_it
 
 
-@dataclass
-class LinkedListItem:
-    value: int
-    previous: "LinkedListItem" = None
-    next: "LinkedListItem" = None
-
-    def __getitem__(self, item):
-        current = self
-        for _ in range(item):
-            current = current.next
-        return current
-
-    def __len__(self):
-        return len(list(iter(self)))
-
-    def __iter__(self):
-        current = self
-        while True:
-            yield current
-            current = current.next
-            if current is self:
-                return
-
-    def find(self, value) -> "LinkedListItem":
-        for candidate in iter(self):
-            if candidate.value == value:
-                return candidate
-        else:
-            raise ValueError(f"Value {value} not found")
-
-    def mix(self):
-        # Zero does not move.
-        if self.value == 0:
-            return
-
-        # Iterate forward or backward. Backward requires an extra iteration because
-        # we want to select the first of the pair in which we want to wedge between.
-        is_negative = self.value < 0
-        start_wedge = self
-        for _ in range(abs(self.value) + is_negative):
-            start_wedge = start_wedge.previous if is_negative else start_wedge.next
-
-        # Cycle does not move.
-        if start_wedge is self:
-            return
-
-        # Stitch up the old spot.
-        self.previous.next = self.next
-        self.next.previous = self.previous
-
-        # Stitch into the new spot.
-        end_wedge = start_wedge.next
-        start_wedge.next = self
-        end_wedge.previous = self
-
-        # u[date own references
-        self.previous = start_wedge
-        self.next = end_wedge
+def _parse(input_: str) -> deque[tuple[int, int]]:
+    return deque((i, int(v)) for i, v in enumerate(input_.split("\n")))
 
 
-def _parse(input_: str) -> list[LinkedListItem]:
-    iterator = iter(input_.split("\n"))
-    head = LinkedListItem(int(next(iterator)))
-    all_items = [head]
-    last = head
-    for ri in iterator:
-        new = LinkedListItem(int(ri))
-        last.next = new
-        new.previous = last
-        last = new
-        all_items.append(new)
-    else:
-        last.next = head
-        head.previous = last
-    return all_items
+def _mix(q: deque, r: int):
+    v = q.popleft()
+    q.rotate(-1 * r)
+    q.appendleft(v)
+
+
+def _put_first(q: deque, v):
+    rota = q.index(v) + 1
+    q.rotate(-1 * (rota - 1))
 
 
 @time_it
 def part_a(input_: str):
     all_items = _parse(input_)
 
-    # Sequence is important in mixing.
-    for item in all_items:
-        item.mix()
+    all_items_copy = copy(all_items)
 
-    zero = all_items[0].find(0)
+    zero_item = None
+    for t in all_items_copy:
+        _put_first(all_items, t)
+        if (rotation := all_items[0][1]) != 0:
+            _mix(all_items, rotation)
+        else:
+            zero_item = t
 
-    # Check that we lost nothing.
-    assert len(zero) == len(all_items)
+    # Rotate to a zero.
+    _put_first(all_items, zero_item)
 
-    # TODO: not -18746, 2342 too low, so answer should be positive!
-    return (
-        zero[1000].value +
-        zero[2000].value +
-        zero[3000].value
-    )
+    result = 0
+    for _ in range(3):
+        all_items.rotate(-1000)
+        print(all_items[0][1])
+        result += all_items[0][1]
+    return result
 
 
 @time_it
