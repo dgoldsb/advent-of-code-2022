a7130376d21bbd2efc4cd70f1f8724717712c65a
dgoldsb <dgoldsb@live.nl>
2022-12-21 14:29:01 +0100

    Faster solution

--- pyo3/aoc/2022/day_21.py
+++ pyo3/aoc/2022/day_21.py
@@ -1,44 +1,88 @@
 """Solve day 21."""
-# TODO: Try tree for faster less hacky way.
-import re
+from dataclasses import dataclass
+from typing import Any
 
 from aoc.helpers import time_it
 
-PURE_EQUATION = re.compile("^[^a-z]+$")
+
+@dataclass
+class Node:
+    name: str
+    operator: str
+    left_child: Any
+    right_child: Any
+
+    @property
+    def linked(self):
+        # Quick litmus test.
+        return not isinstance(self.left_child, str)
+
+    def __bool__(self):
+        return float(self.left_child) == float(self.right_child)
+
+    def __gt__(self, other):
+        return float(self) > float(other)
+
+    def __int__(self):
+        return int(float(self))
+
+    def __float__(self):
+        if self.operator == "+":
+            return float(self.left_child) + float(self.right_child)
+        elif self.operator == "-":
+            return float(self.left_child) - float(self.right_child)
+        elif self.operator == "*":
+            return float(self.left_child) * float(self.right_child)
+        elif self.operator == "/":
+            return float(self.left_child) / float(self.right_child)
 
 
-def _parse(input_: str) -> dict[str, str]:
-    return {l.split(": ")[0]: l.split(": ")[1] for l in input_.splitlines()}
+def _link_node(node: Node, node_dict: dict[str, Node | int]):
+    left = node_dict[node.left_child]
+    right = node_dict[node.right_child]
+    node.left_child = left
+    node.right_child = right
+
+    if isinstance(left, Node) and not left.linked:
+        _link_node(left, node_dict)
+
+    if isinstance(right, Node) and not right.linked:
+        _link_node(right, node_dict)
 
 
-def _get_equation(d: dict[str, str]):
-    while True:
-        if PURE_EQUATION.match(d["root"]):
-            return d["root"]
-        else:
-            for kr, er in d.items():
-                for kt, et in d.items():
-                    d[kt] = et.replace(kr, f"({er})")
+def _parse(input_: str) -> tuple[Node, Node]:
+    name_node_dict: dict[str, Node | float] = {}
+    for line in input_.splitlines():
+        name, eq = line.split(": ")
+        try:
+            left, operator, right = eq.split(" ")
+            name_node_dict[name] = Node(name, operator, left, right)
+        except Exception:
+            name_node_dict[name] = float(eq)
+
+    # For part 2.
+    name_node_dict["humn"] = Node("humn", "+", name_node_dict["humn"], 0)
+    _link_node(name_node_dict["root"], name_node_dict)
+
+    return name_node_dict["root"], name_node_dict["humn"]
 
 
 @time_it
 def part_a(input_: str):
-    return int(eval(_get_equation(_parse(input_))))
+    root, _ = _parse(input_)
+    return int(root)
 
 
 @time_it
 def part_b(input_: str):
-    d = _parse(input_)
-    d["humn"] = "X"
-    d["root"] = d["root"].replace(" + ", " == ")
-    eql, eqr = _get_equation(d).split(" == ")
+    root, human = _parse(input_)
     min_, max_ = 0, 1000_000_000_000_000_000
-    target = eval(eqr)
+
     while True:
-        X = (min_ + max_) // 2
-        if (actual := eval(eql)) == target:
-            return X
-        elif actual > target:
-            min_ = X
+        human.left_child = (min_ + max_) // 2
+        if root:
+            return human.left_child
+        elif root.left_child > root.right_child:
+            min_ = human.left_child
         else:
-            max_ = X
+            max_ = human.left_child
