e9ec38653e7dc268aad5675833165a6565de7117
dgoldsb <dgoldsb@live.nl>
2023-02-12 20:52:33 +0100

    Day 1 in Java, not super happy witht input parser yet

--- java/src/main/java/aoc/aoc2022/Day01.java
+++ java/src/main/java/aoc/aoc2022/Day01.java
@@ -1,13 +1,40 @@
 package aoc.aoc2022;
 
 import aoc.DayInterface;
+import aoc.input.InputParser;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.io.IOException;
 
 public class Day01 implements DayInterface {
-    public String partA() {
-        return "foo";
+    private ArrayList<Integer> getBackpacks() throws IOException {
+        InputParser parser = new InputParser(1, 2022);
+        List<String> lines = parser.readLines().toList();
+
+        int backpack = 0;
+        ArrayList<Integer> backpacks = new ArrayList<>();
+        for (String line : lines) {
+            try {
+                int calories = Integer.parseInt(line);
+                backpack += calories;
+            } catch (NumberFormatException ex) {
+                backpacks.add(backpack);
+                backpack = 0;
+            }
+        }
+        backpacks.add(backpack);
+        return backpacks;
     }
 
-    public String partB() {
-        return "foo";
+    public String partA() throws IOException {
+        return getBackpacks().stream().max(Integer::compare).orElseThrow().toString();
+    }
+
+    public String partB() throws IOException {
+        ArrayList<Integer> backpacks = getBackpacks();
+        backpacks.sort(Collections.reverseOrder());
+        return ((Integer) backpacks.stream().limit(3).mapToInt(Integer::intValue).sum()).toString();
     }
 }
--- java/src/main/java/aoc/input/InputParser.java
+++ java/src/main/java/aoc/input/InputParser.java
@@ -1,4 +1,23 @@
 package aoc.input;
 
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class InputParser {
+    int day;
+    int year;
+    String INPUT_ROOT = "/Users/dylangoldsborough/Repositories/advent-of-code-2022/pyo3/inputs";
+
+    public InputParser(int day, int year) {
+        this.day = day;
+        this.year = year;
+    }
+
+    public Stream<String> readLines() throws IOException {
+        URI fileName = URI.create(("file://%s/%d/%d.txt").formatted(INPUT_ROOT, year, day));
+        return Files.lines(Paths.get(fileName));
+    }
 }
